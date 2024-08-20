753cf7cb4b70732b4722dfd00008b7b75647f405
Saugat <saugatpokharel201@gmail.com>
2024-08-20 18:04:05 +0545

    datatype

---
+++ start/datatype/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class program{
+    static void Main(string[] args){
+        int age=23;
+        Console.WriteLine(age);
+        double negative=-55.2D;
+        System.Console.WriteLine(negative);
+        float precision=5.0000001F;
+        System.Console.WriteLine(precision);
+        decimal money=14.99M;
+        System.Console.WriteLine(money);
+        string name="hari";
+        System.Console.WriteLine(name);
+        char letter='A';
+        System.Console.WriteLine(letter);
+///////----IMP-----------////////
+// string number to real workable number conversion
+string textAge="25";
+int intage=Convert.ToInt32(textAge);
+System.Console.WriteLine(intage);
+
+///////////////////////////////
+///##-----Boolean Data Type ##
+
+bool value=true;
+//1 line of code to offend lgbtq+++ 
+
+bool gender;
+
+
+Console.ReadLine();
+
+    }
+}
