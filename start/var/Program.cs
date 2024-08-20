c8216dd9743291981a9bcbaa921506d2cfc031da
Saugat <saugatpokharel201@gmail.com>
2024-08-20 18:06:06 +0545

    var keywod

---
+++ start/var/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+class program{
+    static void Main(string[] args){
+var num=0;
+  string text=Console.ReadLine();
+  int.TryParse(text,out num);
+  System.Console.WriteLine(num);
+    }
+}
